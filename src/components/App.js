69e781ec41ab5d3295c6aba356e95c4d8235011d
Sean <napolisc10@gmail.com>
2023-02-20 13:46:25 -0700

    commit tree 07cf4dd509482fd0f44357b17f2de46ace7f4b2e

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import SpicyFoodList from "./SpicyFoodList";
 
 function App() {
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleToggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+  
   return (
-    <div>
+    <div className={`App ${darkMode ? "dark": "light"}`}>
+  
       <h3>SpicyFoodList</h3>
+      <button onClick={handleToggleDarkMode}>Toggle Dark Mode</button>
       <SpicyFoodList />
     </div>
   );
